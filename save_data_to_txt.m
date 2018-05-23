5e88c17f6309b56c0eba7bdf107408d5685be38a
RydbergAnalysis2 <RydbergAnalysis2@BRK0B16CHN1SSW1.ad.rice.edu>
2018-05-23 14:35:11 -0500

    can now save several fields to text files using save_data_to_txt.m

--- save_data_to_txt.m
+++ save_data_to_txt.m
@@ -1,22 +1,26 @@
-function [ files ] = save_data_to_text( analyVar, indivDataset, avgDataset )
+function [ files ] = save_data_to_txt( analyVar, indivDataset, avgDataset )
 
     indVarField = 'imagevcoAtom';
-    depVarField = 'winTotNum';
+    depVarField = {'winTotNum','atomTempX','atomTempY','atomTemp','density'};
     
-    [xdata, ydata] = getxy(indVarField, depVarField, analyVar, indivDataset, avgDataset);
-    
-    for i = 1:analyVar.numBasenamesAtom
+    for j = 1:length(depVarField)
         
-        if size(xdata{i}) ~= size(ydata{i})
-            warning(['Dimensions of xdata, ydata not the same. ' ...
-                'Trying to fix, but may lead to unpredictable results.'])
-            ydata{i} = ydata{i}';
+        [xdata, ydata] = getxy(indVarField, depVarField{j}, analyVar, indivDataset, avgDataset);
+        
+        for i = 1:analyVar.numBasenamesAtom
+
+            if size(xdata{i}) ~= size(ydata{i})
+                warning(['Dimensions of xdata, ydata not the same. ' ...
+                    'Trying to fix, but may lead to unpredictable results.'])
+                ydata{i} = ydata{i}';
+            end
+
+
+            outfile = fopen(strcat('./out/',analyVar.basenamevectorAtom{i},'_',depVarField{j},'.txt'),'w');
+            fprintf(outfile, '%0.30e\t%0.30e\n', [xdata{i} ydata{i}]');
+            fclose(outfile);
         end
-       
         
-        outfile = fopen(strcat('./out/',analyVar.basenamevectorAtom{i},'.txt'),'w');
-        fprintf(outfile, '%0.30e\t%0.30e\n', [xdata{i} ydata{i}]');
-        fclose(outfile);
     end
 
 
