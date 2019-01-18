538805395104dc7accf2e115a1b009fc46c580f1
rogding <rogding@users.noreply.github.com>
2019-01-18 14:02:08 -0600

    Got plot "Names" to show in legend.

--- Library/Plugin_Routines/ChargeParticleDetection_Routines/MCS_Integrated_SFI_Spectrum/plot_MCS_Integrated_SFI_Spectrum.m
+++ Library/Plugin_Routines/ChargeParticleDetection_Routines/MCS_Integrated_SFI_Spectrum/plot_MCS_Integrated_SFI_Spectrum.m
@@ -4,10 +4,13 @@ function plot_MCS_Integrated_SFI_Spectrum(analyVar, indivDataset)
     hold on;
     for i = 1:analyVar.numBasenamesAtom
         
-        plot(indivDataset{i}.imagevcoAtom, indivDataset{i}.sfiIntegral, analyVar.MARKERS{i}, 'Color', analyVar.COLORS(i,:), 'MarkerSize', analyVar.markerSize)
-        xlabel('GHz synth. [MHz]')
-        ylabel('Total MCS Counts')
-        
+        plot(indivDataset{i}.imagevcoAtom, indivDataset{i}.sfiIntegral,...
+            analyVar.MARKERS{i}, 'Color', analyVar.COLORS(i,:),...
+            'MarkerSize', analyVar.markerSize,...
+            'DisplayName',num2str(analyVar.timevectorAtom(i)));
+        xlabel('GHz synth. [MHz]');
+        ylabel('Total MCS Counts');
     end
-    
+    legend('show', 'Location', 'best');
+    hold off;    
 end
