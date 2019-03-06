eae984df20e745c9db80daef6bd0cde130c5bf86
kanungo92 <35298501+kanungo92@users.noreply.github.com>
2019-03-06 09:21:58 -0600

    reads in ROIs from AnalyVar

--- Library/Parameter_Extraction/param_extract_sfi_integral_roi_select.m
+++ Library/Parameter_Extraction/param_extract_sfi_integral_roi_select.m
@@ -5,10 +5,10 @@ function indivDataset = param_extract_sfi_integral_roi_select(analyVar, indivDat
         indivDataset{i}.sfiIntegral_roi1 = zeros(indivDataset{i}.CounterMCS,1); %initalize sum vector
         indivDataset{i}.sfiIntegral_roi2 = zeros(indivDataset{i}.CounterMCS,1); %initalize sum vector
         for j = 1:indivDataset{i}.CounterMCS % loop over mcs files
-            roi1_min = 110;
-            roi1_max = 150;
-            roi2_min = 151;
-            roi2_max = 200;
+            roi1_min = analyVar.roi1_minimum;
+            roi1_max = analyVar.roi1_maximum;
+            roi2_min = analyVar.roi2_minimum;
+            roi2_max = analyVar.roi2_maximum;
             indivDataset{i}.sfiIntegral_roi1(j) = sum(indivDataset{i}.mcsSpectra{j}(roi1_min:roi1_max,2));
             indivDataset{i}.sfiIntegral_roi2(j) = sum(indivDataset{i}.mcsSpectra{j}(roi2_min:roi2_max,2)); 
             
