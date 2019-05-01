e3f4b634bf37cdf84ac0ee561ec5feb89ac10d91
RydbergAnalysis2 <RydbergAnalysis2@BRK0B16CHN1SSW1.ad.rice.edu>
2019-05-01 12:29:11 -0500

    added more data columns

--- save_data_to_txt_ver2.m
+++ save_data_to_txt_ver2.m
@@ -2,6 +2,8 @@ function [ files ] = save_data_to_txt_ver2( analyVar, indivDataset, avgDataset )
 
     indVarField = 'imagevcoAtom';
     depVarField = {'sfiIntegral'};
+    depVarField1 = {'sfiIntegral_roi1'}; % FOR mm-wave stuff
+    depVarField2 = {'sfiIntegral_roi2'}; % FOR mm-wave stuff
     if analyVar.UseImages==1
         tempXfield= {'atomTempX'};
         tempYfield= {'atomTempY'};
@@ -17,6 +19,7 @@ function [ files ] = save_data_to_txt_ver2( analyVar, indivDataset, avgDataset )
         [xdata, ydata] = getxy(indVarField, depVarField{j}, analyVar, indivDataset, avgDataset);
         [tempx, tempy] = getxy(tempXfield{j}, tempYfield{j}, analyVar, indivDataset, avgDataset);
         [~, num]   = getxy(indVarField, numfield{j}, analyVar, indivDataset, avgDataset);
+        [ydata1, ydata2] = getxy(depVarField1{j},depVarField2{j}, analyVar, indivDataset, avgDataset);
         for i = 1:analyVar.numBasenamesAtom
 
             if size(xdata{i}) ~= size(ydata{i})
@@ -27,7 +30,7 @@ function [ files ] = save_data_to_txt_ver2( analyVar, indivDataset, avgDataset )
 
 
             outfile = fopen(strcat('./out/',analyVar.dataDirName,analyVar.basenamevectorAtom{i},'_',depVarField{j},'.txt'),'w');
-            fprintf(outfile, '%0.30e\t%0.30e\t%0.30e\t%0.30e\t%0.30e\n', [xdata{i} ydata{i} tempx{i} tempy{i} num{i}]');
+            fprintf(outfile, '%0.30e\t%0.30e\t%0.30e\t%0.30e\t%0.30e\t%0.30e\t%0.30e\n', [xdata{i} ydata{i} tempx{i} tempy{i} num{i} ydata1{i} ydata2{i}]');
             fclose(outfile);
         end
         
