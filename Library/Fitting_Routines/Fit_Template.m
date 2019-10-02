73e5197c7fe2226a68d7f0b49046af4734a17f52
RydbergAnalysis2 <RydbergAnalysis2@BRK0B16CHN1SSW1.ad.rice.edu>
2019-10-02 14:11:14 -0500

    added some documentation to fit_template

--- Library/Fitting_Routines/Fit_Template.m
+++ Library/Fitting_Routines/Fit_Template.m
@@ -1,12 +1,44 @@
 function funcOut = Fit_Template(analyVar, indivDataset, avgDataset)
-
-    form = @(coeffs, x)coeffs(3)*sin(2*pi*coeffs(1)/1000*x+coeffs(6))+...
-        sin(2*pi*coeffs(2)/1000*x+coeffs(7))*coeffs(4)+coeffs(5);
     
-    indVarField = 'imagevcoAtom';
-    depVarField = 'cntrX';
+    %% Fit_Template - Joe Whalen 2019.10.02
+    % This function calls the base_fit script that does all of the fitting
+    % and displays all of the plots for a given fitting routine. To use
+    % this template, first save it as a new script with the name you want
+    % to give to your fit function. Next, fill in your fit form with an
+    % anonymous function handle with argumnents coeffs and x, coeffs is the
+    % vector of free fit parameters and x is the vector of x coordinates
+    % over which the function will be evaluated. Declare the independent
+    % and dependent variables to be fitted with indVarField and
+    % depVarField. These variables are strings that are the names of a
+    % field in indivDataset. Typically the indVarField is imagevcoAtom, the
+    % variable that was scanned during the experiment.
+    
+    form = @(coeffs, x) % your fit function here 
+    
+    indVarField = 'imagevcoAtom'; % independent variable
+    depVarField = 'cntrX'; % dependent variable
+    
+    %% initial guess code
+    % fill in this function to estimate the values of the fit parameters,
+    % alternatively you can just have thist function return a constant
+    % vector if you don't have a simple way of obtaining an initial guess
+    function initialguess = x0(xdata, ydata)
+        % code
+        % that
+        % guesses
+        % initial
+        % params
+        % can also return a constant vector with length equal to the number
+        % of parameters in the fit function
+        initialguess = [];
+    end
 
-    options = struct(...
+
+    %% options
+    % the base_fit function is designed to be flexible, and can accept a
+    % lot of different parameters to adjust 
+    %%%%% Default Options %%%%%
+    %options = struct(...
         %'DataPlotFunction', @defaultDataPlot,...
         %'AvgDataPlotFunction', @defaultAvgDataPlot,...
         %'FitLinePlotFunction', @defaultFitLinePlot,...
@@ -19,7 +51,9 @@ function funcOut = Fit_Template(analyVar, indivDataset, avgDataset)
         %'FitLB', [],...
         %'FitUB', [],...
         %'FitOptions', struct('Display','off') );
-        );
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    options = struct();
     
     base_fit(analyVar, indivDataset, avgDataset, form, indVarField, depVarField, @x0, options)
 
@@ -29,6 +63,3 @@ function funcOut = Fit_Template(analyVar, indivDataset, avgDataset)
 
 end
 
-function initialguess = x0(xdata, ydata)
-    initialguess = [14,32,1,1,100,0,0];
-end
