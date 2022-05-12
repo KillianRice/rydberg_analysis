128750498c599124bccad602ac7a5e5b92a7eb23
Soumya K Kanungo <skk4@rice.edu>
2022-05-12 16:33:23 -0500

    added the avg and std annotations with color text
    added the avg and std annotations with color text

--- Library/Plugin_Routines/avg_num_and_temp.m
+++ Library/Plugin_Routines/avg_num_and_temp.m
@@ -41,10 +41,12 @@ function funcOut = avg_num_and_temp(analyVar, indivDataset, avgDataset)
             hold on;
             for id = 1:length(scanIDs)
                 histogram(groupvectors{i}{id});
+                myAnnotation(fields{i}, mean(groupvectors{i}{id}), std(groupvectors{i}{id}),id);
             end
             xlabel(fields{i});
             ylabel('Occurances');
             legend(num2str(scanIDs));
+            
 
         end
         
@@ -70,7 +72,7 @@ function funcOut = avg_num_and_temp(analyVar, indivDataset, avgDataset)
             histogram(allvectors{i})
             xlabel(fields{i});
             ylabel('Occurances');
-            myAnnotation(fields{i}, mean(allvectors{i}), std(allvectors{i}));
+            myAnnotation(fields{i}, mean(allvectors{i}), std(allvectors{i}),0);
         end
         
     end
@@ -81,14 +83,22 @@ function funcOut = avg_num_and_temp(analyVar, indivDataset, avgDataset)
 
 end
 
-function an = myAnnotation(field,mean,std)
-    dim = [.2 .5 .3 .3];
+function an = myAnnotation(field,mean,std,iter)
+    dim = [.1 + (iter-1)*0.3 .5 .3 .3];
     fmt = '%0.5e';
     str_label = strcat(field);
     str_mean = strcat('Mean: ',num2str(mean,fmt));
     str_std = strcat('Std Dev: ',num2str(std,fmt));
     str = [str_label, char(10),str_mean, char(10), str_std];
-    an = annotation('textbox',dim,'String',str,'FitBoxToText','on','BackgroundColor','white');
+    COLORS  = [    0    0.4470    0.7410
+    0.8500    0.3250    0.0980
+    0.9290    0.6940    0.1250
+    0.4940    0.1840    0.5560
+    0.4660    0.6740    0.1880
+    0.3010    0.7450    0.9330
+    0.6350    0.0780    0.1840];
+    an = annotation('textbox',dim,'String',str,'FitBoxToText','on','BackgroundColor',...
+        'white','Color',COLORS(iter,:));
 end
 
 function h = myplot(x,y,analyVar,i)
